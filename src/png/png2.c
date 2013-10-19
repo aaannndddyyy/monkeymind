4070b527c316e1e0d6db7b7b62b20b7f215c3887
Bob Mottram <bob@robotics.uk.to>
2013-10-19 17:17:41 +0100

    Fix png2 warning

--- src/png/png2.c
+++ src/png/png2.c
@@ -29,14 +29,22 @@
 
 ****************************************************************/
 
-#ifndef PNG2_H
-#define PNG2_H
+#include "png2.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <zlib.h>
-#include "pnglite.h"
+int write_png_file(char* filename, int width, int height, unsigned char *buffer)
+{
+  png_t png;
+  FILE * fp = fopen(filename, "wb");
+  if (fp == NULL) {
+    fprintf(stderr, "Could not open file %s for writing\n", filename);
+    return 1;
+  }
+  fclose(fp);
 
-int write_png_file(char* filename, int width, int height, unsigned char *buffer);
+  png_init(0,0);
+  png_open_file_write(&png, filename);
+  png_set_data(&png, width, height, 8, PNG_TRUECOLOR, buffer);
+  png_close_file(&png);
 
-#endif
+  return 0;
+}
