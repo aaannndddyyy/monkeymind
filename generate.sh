fc2086dc1406d49f089a522b4cbab6175db3023a
Bob Mottram <bob@robotics.uk.to>
2014-05-03 13:20:16 +0100

    Change username

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@
 
 rm -f Makefile rpmpackage/*.spec
 
-packagemonkey -n "monkeymind" --cmd --dir "." -l "mit" -e "Bob Mottram (4096 bits) <bob@robotics.uk.to>" --brief "Mind of the monkey" --desc "Experiments with implementing a new cognitive system for multi-agent simulations" --homepage "https://github.com/fuzzgun/monkeymind" --repository "https://github.com/fuzzgun/monkeymind.git" --section "utils" --version "0.1" --categories "Utility/ConsoleOnly" --compile "src/png/*.c -lz"
+packagemonkey -n "monkeymind" --cmd --dir "." -l "mit" -e "Bob Mottram (4096 bits) <bob@robotics.uk.to>" --brief "Mind of the monkey" --desc "Experiments with implementing a new cognitive system for multi-agent simulations" --homepage "https://github.com/bashrc/monkeymind" --repository "https://github.com/bashrc/monkeymind.git" --section "utils" --version "0.1" --categories "Utility/ConsoleOnly" --compile "src/png/*.c -lz"
